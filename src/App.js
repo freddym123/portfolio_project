1dfdbbb344f30347d38af0f7fb98b7ac1da5b60e
Freddy Martinez <freddy234@gmail.com>
2024-01-03 17:26:05 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,115 @@
 import logo from './logo.svg';
 import './App.css';
+import Hero from './components/Hero';
+import About from './components/About';
+import editor from "./images/editor.png"
+import chatapp from "./images/chatapp.png"
+import recipeapp from "./images/recipeapp.png"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Hero></Hero>
+
+      <main>
+        <About></About>
+
+        <div>
+          <h2 className='sub-title second'>Some Things I've Built</h2>
+          <div className='projects'>
+            <div className='project'>
+              <div className='image-container' data-aos="fade-right"><img src={editor}></img></div>
+              <div className='project-text' data-aos="fade-left">
+                <p className='featured'>Featured Project</p>
+                <h3>Javascript editor</h3>
+                <div className='desc'>
+                  <div className='desc-text'>
+                    A javascript web editor which sends code to a server and compiles it and returns the output.
+                    It features color coded keywords like some popular editors.
+                  </div> 
+                  <div className='desc-footer'>
+                    <ul>
+                      <li>Vanilla Javascript</li>
+                      <li>CSS</li>
+                      <li>HTML</li>
+                      <li>Node.js</li>
+                    </ul>
+                    <div className='links'>
+                      <div><a href="https://github.com/freddym123/src"><i className="fa-brands fa-github"></i></a></div>
+                      <div><i className="fa-solid fa-up-right-from-square"></i></div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className='project'>
+            <div className='project-text' data-aos="fade-right">
+                <p className='featured'>Featured Project</p>
+                <h3>Chat App</h3>
+                <div className='desc'>
+                  <div className='desc-text'>
+                    A messaging application that allows one to one communication through the use of sockets. The applications features a friend request and friend list feature. User is able to click through chats to text their contacts.
+                  </div> 
+                  <div className='desc-footer'>
+                    <ul>
+                      <li>React Js</li>
+                      <li>CSS</li>
+                      <li>MongoDB</li>
+                      <li>Node.js</li>
+                    </ul>
+                    <div className='links'>
+                      <div><i className="fa-brands fa-github"></i></div>
+                      <div><i className="fa-solid fa-up-right-from-square"></i></div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className='image-container' data-aos="fade-left"><img src={chatapp}></img></div>
+              
+            </div>
+            <div className='project'>
+              <div className='image-container' data-aos="fade-right"><img src={recipeapp}></img></div>
+              <div className='project-text' data-aos="fade-left">
+              <p className='featured'>Featured Project</p>
+                <h3>Recipe App</h3>
+                <div className='desc'>
+                  <div className='desc-text'>
+                    A recipe application that shows popular recipes. First it shows the ingridients that are needed, then user can decide if they want to cook it. User can then save recipes if they are logged In.
+                  </div>
+                  <div className='desc-footer'>
+                    <ul>
+                      <li>React Js</li>
+                      <li>CSS</li>
+                      <li>Python</li>
+                      <li>Node.js</li>
+                    </ul>
+                    <div className='links'>
+                      <div><i className="fa-brands fa-github"></i></div>
+                      <div><i className="fa-solid fa-up-right-from-square"></i></div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+          </div>
+        </div>
+
+        <div className='formWrapper' data-aos="fade-up">
+          <h2 className='sub-title last'>Get In Touch</h2>
+          <p>Here is my contact information to get in touch with me.</p>
+          <p>Thank you for taking some time out of your day going through my portfolio and I hope we get in touch soon.</p>
+          <div className='contactInfo'>
+            <div>
+              Email: <a href="mailto:freddymartinez567767@gmail.com">freddymartinez567767@gmail.com</a>
+            </div>
+            <div>
+              Phone Number: (323)674-5796
+            </div>
+          </div>
+        </div>
+        
+      </main>
     </div>
   );
 }
---
+++ src/components/About.jsx
@@ -0,0 +1,21 @@
+export default function About(){
+    return(<div id="aboutMe">
+    <h2 className="sub-title first">About Me</h2>
+    <div>
+      <p className="about">Hello! My name is Freddy and I enjoy creating applications. My interest in programming started in my last year of high school. I didn't know what I wanted to do after high school but luckily, I was introduced to python in a after school program I would attend. I would then learn more languages like C and C++ and Javascript. From then on I started to program and learn new things almost everyday.</p>
+      <p className="about">
+        Fast-forward to today, I have been fortunate enough to be attending a University where I am pursuing a Bachelor degree in computer science. Some of the things I like to do when I am not programming are playing sports with friends such as soccer and basketball, playing video games, and much more. 
+      </p>
+      <div className="recentTools">
+        <p>Here are a few technologies I've been working with recently:</p>
+        <ul className='workTools'>
+          <li>Javascript</li>
+          <li>React</li>
+          <li>Node.js</li>
+          <li>Python</li>
+        </ul>
+      </div>
+
+    </div>
+  </div>)
+}
